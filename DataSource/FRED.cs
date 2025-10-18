d0632440e53e464d10e2e63e84309f3873e174bf
Patience Fuglo <pfuglo@fordham.edu>
2025-10-18 02:27:12 -0400

    Implement FRED dataset (CPIAUCSL): data model, mapping, toy data

--- DataSource/FRED.cs
+++ DataSource/FRED.cs
@@ -1,156 +1,58 @@
-﻿/*
- * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
- * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
-*/
-
 using System;
-using NodaTime;
-using ProtoBuf;
-using System.IO;
-using QuantConnect.Data;
 using System.Collections.Generic;
+using System.Globalization;
+using NodaTime;
+using QuantConnect.Data;
+using QuantConnect.Logging;
 
 namespace QuantConnect.DataSource
 {
-    /// <summary>
-    /// Example custom data type
-    /// </summary>
-    [ProtoContract(SkipConstructor = true)]
-    public class MyCustomDataType : BaseData
+    // Single-file reader: data/alternative/fred/<series>.csv (YYYY-MM-DD,value)
+    public class FRED : BaseData
     {
-        /// <summary>
-        /// Some custom data property
-        /// </summary>
-        [ProtoMember(2000)]
-        public string SomeCustomProperty { get; set; }
+        public decimal Observation { get; set; }
+        public string SeriesId { get; set; }
 
-        /// <summary>
-        /// Time passed between the date of the data and the time the data became available to us
-        /// </summary>
-        public TimeSpan Period { get; set; } = TimeSpan.FromDays(1);
-
-        /// <summary>
-        /// Time the data became available
-        /// </summary>
-        public override DateTime EndTime => Time + Period;
-
-        /// <summary>
-        /// Return the URL string source of the file. This will be converted to a stream
-        /// </summary>
-        /// <param name="config">Configuration object</param>
-        /// <param name="date">Date of this source file</param>
-        /// <param name="isLiveMode">true if we're in live mode, false for backtesting mode</param>
-        /// <returns>String URL of source file.</returns>
         public override SubscriptionDataSource GetSource(SubscriptionDataConfig config, DateTime date, bool isLiveMode)
         {
-            return new SubscriptionDataSource(
-                Path.Combine(
-                    Globals.DataFolder,
-                    "alternative",
-                    "mycustomdatatype",
-                    $"{config.Symbol.Value.ToLowerInvariant()}.csv"
-                ),
-                SubscriptionTransportMedium.LocalFile
-            );
+            var seriesId = FredSeries.Resolve(config.Symbol.Value);
+            var path = $"data/alternative/fred/{seriesId}.csv";
+            Log.Debug($"[GETSOURCE] {config.Symbol.Value} -> {path}");
+            return new SubscriptionDataSource(path, SubscriptionTransportMedium.LocalFile, FileFormat.Csv);
         }
 
-        /// <summary>
-        /// Parses the data from the line provided and loads it into LEAN
-        /// </summary>
-        /// <param name="config">Subscription configuration</param>
-        /// <param name="line">Line of data</param>
-        /// <param name="date">Date</param>
-        /// <param name="isLiveMode">Is live mode</param>
-        /// <returns>New instance</returns>
         public override BaseData Reader(SubscriptionDataConfig config, string line, DateTime date, bool isLiveMode)
         {
-            var csv = line.Split(',');
+            if (string.IsNullOrWhiteSpace(line)) return null;
+            var parts = line.Split(',');
+            if (parts.Length < 2) return null;
 
-            var parsedDate = Parse.DateTimeExact(csv[0], "yyyyMMdd");
-            return new MyCustomDataType
+            var time  = DateTime.Parse(parts[0].Trim(), CultureInfo.InvariantCulture);
+            var value = Convert.ToDecimal(parts[1].Trim(), CultureInfo.InvariantCulture);
+
+            return new FRED
             {
-                Symbol = config.Symbol,
-                SomeCustomProperty = csv[1],
-                Time = parsedDate - Period,
+                Symbol      = config.Symbol,
+                Time        = time,
+                EndTime     = time.AddDays(1),
+                SeriesId    = FredSeries.Resolve(config.Symbol.Value),
+                Observation = value,
+                Value       = value
             };
         }
 
-        /// <summary>
-        /// Clones the data
-        /// </summary>
-        /// <returns>A clone of the object</returns>
-        public override BaseData Clone()
-        {
-            return new MyCustomDataType
-            {
-                Symbol = Symbol,
-                Time = Time,
-                EndTime = EndTime,
-                SomeCustomProperty = SomeCustomProperty,
-            };
-        }
+        public override DateTimeZone DataTimeZone()             => TimeZones.Utc;
+        public override List<Resolution> SupportedResolutions() => DailyResolution;
+        public override Resolution DefaultResolution()          => Resolution.Daily;
+        public override bool IsSparseData()                     => true;
+        public override bool RequiresMapping()                  => false;
 
-        /// <summary>
-        /// Indicates whether the data source is tied to an underlying symbol and requires that corporate events be applied to it as well, such as renames and delistings
-        /// </summary>
-        /// <returns>false</returns>
-        public override bool RequiresMapping()
+        public override BaseData Clone() => new FRED
         {
-            return true;
-        }
+            Symbol = Symbol, Time = Time, EndTime = EndTime,
+            SeriesId = SeriesId, Observation = Observation, Value = Value
+        };
 
-        /// <summary>
-        /// Indicates whether the data is sparse.
-        /// If true, we disable logging for missing files
-        /// </summary>
-        /// <returns>true</returns>
-        public override bool IsSparseData()
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// Converts the instance to string
-        /// </summary>
-        public override string ToString()
-        {
-            return $"{Symbol} - {SomeCustomProperty}";
-        }
-
-        /// <summary>
-        /// Gets the default resolution for this data and security type
-        /// </summary>
-        public override Resolution DefaultResolution()
-        {
-            return Resolution.Daily;
-        }
-
-        /// <summary>
-        /// Gets the supported resolution for this data and security type
-        /// </summary>
-        public override List<Resolution> SupportedResolutions()
-        {
-            return DailyResolution;
-        }
-
-        /// <summary>
-        /// Specifies the data time zone for this data type. This is useful for custom data types
-        /// </summary>
-        /// <returns>The <see cref="T:NodaTime.DateTimeZone" /> of this data type</returns>
-        public override DateTimeZone DataTimeZone()
-        {
-            return DateTimeZone.Utc;
-        }
+        public override string ToString() => $"{SeriesId}:{Time:yyyy-MM-dd}={Observation}";
     }
 }
